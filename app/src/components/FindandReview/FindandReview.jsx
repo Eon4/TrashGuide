9f8e87972b26215abfe3cec5777fc74b89f0675f
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-03-04 20:30:25 +0100

    Update 4 - Error fixed
    - vision was not correct, error is fixed.

--- app/src/components/FindandReview/FindandReview.jsx
+++ app/src/components/FindandReview/FindandReview.jsx
@@ -1,37 +1,37 @@
 
 // Find and Review component with two links for find station and login prop component
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import style from './FindandReview.module.scss';
 
-const FindAndReviewMe = () => {
-  const history = useHistory();
+export const FindAndReviewMe = () => {
+  const navigate = useNavigate();
 
   const handleFindStationClick = () => {
-    history.push('/recycling');
+    navigate('/recycling');
   };
 
   const handleLoginClick = () => {
-    history.push('/login');
+    navigate('/login');
   };
 
   return (
-<div className={style.FindAndReviewContainer}>
-  <h2>Find og anmeld genbrugsstationer</h2>
-  <li>
-    <button onClick={handleFindStationClick}>
-      Find station
-    </button>
-  </li>
-  <li>
-    <button onClick={handleLoginClick}>
-      Log ind
-    </button>
-  </li>
-</div>
-
+    <div className={style.FindAndReviewContainer}>
+      <h2>Find og anmeld genbrugsstationer</h2>
+      <li>
+        <button onClick={handleFindStationClick}>
+          Find station
+        </button>
+      </li>
+      <li>
+        <button onClick={handleLoginClick}>
+          Log ind
+        </button>
+      </li>
+    </div>
   );
 }
 
 export default FindAndReviewMe;
+
 
