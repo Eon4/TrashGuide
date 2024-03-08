442decc357b0d5b3ce9de30ee12785efb56b6a21
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-03-08 12:52:16 +0100

    Update 17 - OrderPage
    - Styling updated with mobile quires.

--- app/src/pages/OrderPage/OrderPage.jsx
+++ app/src/pages/OrderPage/OrderPage.jsx
@@ -1,12 +1,13 @@
-// Order Page One
 import { useEffect, useState } from 'react';
 import style from './OrderPage.module.scss';
 import { ShowMyContainerCard } from '../../components/ContainerCard/ContainerCard';
 import { NavLink } from 'react-router-dom';
 
 export const OrderPage = () => {
+  // A state to store the available order container data
   const [orderContainer, setOrderContainer] = useState([]);
 
+  // useEffect to fetch order container data when the component mounts
   useEffect(() => {
     fetch('http://localhost:3000/containers')
       .then((res) => res.json())
@@ -16,34 +17,31 @@ export const OrderPage = () => {
 
   return (
     <div className={style.OrderMyContainer}>
-      {/* <div>
-        <aside>
-          Breaddrumb container here
-        </aside>
-      </div> */}
       <p className={style.stepText} >Trin 1</p>
       <h3 className={style.containerHeadline} >Vælg type</h3>
       <p>Tation argumentum et usu, dicit viderer evertitur te has. 
         Eu dictas <br /> concludaturque usu, facete detracto patrioque 
         an per, lucilius <br /> pertinacia eu vel.</p>
-        <div className={style.OrderMyContainerWrapper} >
+        
+      {/* Wrapper for displaying order container cards */}
+      <div className={style.OrderMyContainerWrapper} >
+        {/* Mapping over the orderContainer array and rendering ShowMyContainerCard components */}
+        {orderContainer.map((container) => (
+          <ShowMyContainerCard 
+            key={container.id} 
+            id={container.id} 
+            name={container.name} 
+            icon={container.icon_filename} 
+          />
+        ))}
+      </div>
 
-      {orderContainer.map((container) => (
-        <ShowMyContainerCard 
-        key={container.id} 
-        id={container.id} 
-        name={container.name} 
-        icon={container.icon_filename} 
-        />
-      ))}
-              </div>
-
-  <NavLink to="/sendorder">
-    <button className={style.orderButton}>
-    Videre
-
-    </button>
-</NavLink>  
-</div>
+      {/* Button to navigate to the next step in the order process */}
+      <NavLink to="/sendorder">
+        <button className={style.orderButton}>
+          Videre
+        </button>
+      </NavLink>  
+    </div>
   );
 };
