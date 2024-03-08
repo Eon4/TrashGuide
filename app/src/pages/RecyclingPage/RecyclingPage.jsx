9cb44c4987123806c12da97805ff77a0e64d1548
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-03-08 12:22:36 +0100

    Update 16 - RecyclingPage
    - Mobile quires added and testing.

--- app/src/pages/RecyclingPage/RecyclingPage.jsx
+++ app/src/pages/RecyclingPage/RecyclingPage.jsx
@@ -1,22 +1,24 @@
-// RecyclingPage component
 import style from './RecyclingPage.module.scss';
 import { useEffect, useState } from 'react';
 import { RecyclingCard } from '../../components/RecyclingCard/RecyclingCard';
 import { useNavigate } from 'react-router-dom';
 
 export const RecyclingPage = () => {
+  // A state to store the fetched recycling data
   const [sortedRecycle, setSortedRecycle] = useState([]);
+
   const navigate = useNavigate();
 
-
+  // useEffect to fetch recycling data when the component mounts
   useEffect(() => {
-    // Fetch recycling data from the API
+    // Fetching recycling data from the API
     fetch('http://localhost:3000/orgs?attributes=id,name,address,zipcode,city')
       .then((res) => res.json())
       .then((data) => setSortedRecycle(data))
       .catch((err) => console.error(err));
   }, []);
 
+  // A function to handle clicking on a recycling card and navigate to details page
   const handleClick = (id) => {
     // Navigate to RecyclingPageDetails with the selected ID
     navigate(`/recycling/${id}`);
